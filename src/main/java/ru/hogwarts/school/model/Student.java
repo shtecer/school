3e20765d60848b08f56cdf35da6575a35900a720
Алексей <shtecer@mail.ru>
2025-08-15 13:27:31 +0500

    1

---
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.model;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+
+    public Student(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        }
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public int getAge() {return age;}
+    public void setAge(int age) {this.age = age;}
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (id != student.id) return false;
+        if (age != student.age) return false;
+        return name.equals(student.name);
+    }
+
+}
